21161292d94ab5bbe3a103a46a0c7ba8dd208996
furnapso <luke.boland95@gmail.com>
2021-01-25 22:31:26 +1100

    feat: add task factory

---
+++ src/task.js
@@ -0,0 +1,9 @@
+const Task = (title, description, dueDate, priority) => {
+    let title = title;
+    let description = description;
+    let dueDate = dueDate;
+    let priority = priority;
+
+    const updateTitle = (newTitle) => title = newTitle;
+    const updateDescription = (newDescription) => description = newDescription;
+}
