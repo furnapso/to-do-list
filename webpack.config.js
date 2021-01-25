13d55f41565a0b82dbaf7b5e13f3bfd64d6c630d
furnapso <luke.boland95@gmail.com>
2021-01-25 21:45:38 +1100

    chore: fomantic-ui and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
+  watch: true,
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true
   },
   module: {
     rules: [
@@ -15,8 +21,37 @@ const config = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /.*\.png/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ]
       }
     ]
+  },
+  resolve: {
+    alias: {
+      'fomantic-ui': path.resolve(__dirname, './node_modules/fomantic-ui-css/semantic.min.css')
+    }
   }
 };
 
