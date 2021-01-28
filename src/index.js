57f07c76df5e74e633fae86005368a938c53a37a
furnapso <luke.boland95@gmail.com>
2021-01-28 19:09:31 +1100

    feat: add default board function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import "fomantic-ui"
-import "./styles.css"
+import "fomantic-ui";
+import "./styles.css";
+import Board from "./board";
+import Project from "./project";
+import Task from "./task";
+import UserInterface from "./ui";
+
+function createDefaultBoard() {
+    const defaultBoard = Board();
+    defaultBoard.addProject("Default Project", "This is the default starting project");
+}
