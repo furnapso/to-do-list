a73c007fce5f28adfbedf06b1cae550341b48347
furnapso <luke.boland95@gmail.com>
2021-01-29 12:44:56 +1100

    feat: chnage board to module

--- src/board.js
+++ src/board.js
@@ -1,15 +1,18 @@
 import Project from "./project"
 
-const Board = () => {
+const Board = (() => {
     let projects = [];
     
     const addProject = (title, description) => {
         projects.push(Project(title, description))
     }
+
+    /* create default project */
+    addProject("Default Project", "This is the default starting project");
     
     return {
         projects, addProject
     }
-}
+})();
 
 export default Board;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import "fomantic-ui";
 import "./styles.css";
 import Board from "./board";
-import Project from "./project";
-import Task from "./task";
-import UserInterface from "./ui";
 
-function createDefaultBoard() {
-    const defaultBoard = Board();
-    defaultBoard.addProject("Default Project", "This is the default starting project");
-}
+console.log(Board);
