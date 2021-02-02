9e7b0f0df86417aec8f9f40bf9fd9af575dad179
furnapso <luke.boland95@gmail.com>
2021-02-02 22:12:39 +1100

    feat: multiple changes to project.js add jsdoc hinting update updateProject function to validate updateobj

--- src/project.js
+++ src/project.js
@@ -1,21 +1,45 @@
 // @ts-check
 import Task from "./task";
 
+/**
+ * @param {string} title Project title
+ * @param {string} description Project description
+ */
 const Project = (title, description) => ({
     title: title,
     description: description,
     _tasks: [],
+    _acceptedUpdates: ['title', 'description'],
 
+    /**
+     * @param {string} title New task title
+     * @param {string} description New task description
+     * @param {Date} dueDate New task due date - defaults to today
+     * @param {Number} priority - New task priority - defaults to 1
+     */
     addTask(title, description, dueDate, priority) {
         this._tasks.push(Task(title, description, dueDate, priority));
     },
 
-    updateProject(update) {
-        for (let item in update) {
-            if (Object.keys(this).includes(item)) {
-                this[item] = update[item];
+    /**
+     * @param {Object} updateObj Update object in format {field: new value}. Accepts title & description
+     */
+    updateProject(updateObj) {
+        const invalidUpdates = [];
+        const validUpdates = [];
+        for (let item in updateObj) {
+            if (this._acceptedUpdates.includes(item)) {
+                validUpdates.push(item)
+            }
+            else {
+                invalidUpdates.push(item)
             }
         }
+
+        if (invalidUpdates.length > 0) {
+            let exceptionString = ''
+        }
+
     }
 })
 
