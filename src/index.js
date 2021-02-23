4d95cc14305144b1217d7f32930ea7e78480e08f
furnapso <luke.boland95@gmail.com>
2021-02-24 08:19:11 +1100

    wip: update ui to use lit-html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./styles.css";
 import "fomantic-ui";
+import {render} from "lit-html";
+
 import Board from "./board";
 import UI from "./ui";
 
 console.log(Board.projects);
 Board.projects[0].addTask("New task", "Task description", new Date(), 1);
-let mydiv = UI.components.task(Board.projects[0]._tasks[0]);
-document.querySelector("#tasks").appendChild(mydiv);
+let mydiv = UI.components.tasks([Board.projects[0]._tasks[0], Board.projects[0]._tasks[0]]);
+console.log(mydiv);
+render(mydiv, document.querySelector("#tasks"));
--- src/ui.js
+++ src/ui.js
@@ -1,31 +1,33 @@
+import {html} from "lit-html";
+
 const UserInterface = (() => {
     const projects = document.querySelector("#projects");
     const addNewProjectBtn = document.querySelector("#add-new-project");
     const projectTitle = document.querySelector("#project-title");
 
     const components = {
-        task: (task) => {
-            const tempDiv = document.createElement("div");
-            tempDiv.innerHTML = `
-            <div id='task' class='ui segment'>
+        tasks: (tasks) => {
+            const eventHandler = e => {
+                switch(e.type) {
+                    case "mouseover":
+                        e.target.querySelector("#actions").classList.remove('hidden');
+                        break;
+                    case "mouseleave":
+                        e.target.querySelector("#actions").classList.add('hidden');
+                        break
+                }
+            }
+
+            const div = tasks.map(task => html`
+            <div id='task' class='ui segment' @mouseover=${eventHandler} @mouseleave=${eventHandler}>
                 <input type='checkbox' class='ui checkbox circular'>
                 <div class='task-title'>${task.title}</div>
                 <span id='actions' class='hidden'>
                     <i class='icon delete'></i>
                 </span>
-            </div>`
+            </div>`);
 
-            const div = tempDiv.firstElementChild;
-            
-            div.addEventListener('mouseover', (e) => {
-                e.target.querySelector("#actions").classList.remove("hidden");
-            })
-
-            div.addEventListener('mouseleave', e => {
-                e.target.querySelector("#actions").classList.add("hidden");
-            })
-            
-            return div;
+            return div
         }
     }
 
