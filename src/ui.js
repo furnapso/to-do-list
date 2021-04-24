85ac203b396128e75895ad2306ee72ba2f6395db
furnapso <luke.boland95@gmail.com>
2021-04-24 15:09:04 +1000

    feat: update task ui to be input instead of div

--- src/ui.js
+++ src/ui.js
@@ -67,7 +67,7 @@ const UserInterface = Board => (() => {
             const div = tasks.map(task => html`
             <div id='task' class='ui segment ${task.completed ? 'strike' : ''}' @mouseover=${taskEventHandler} @mouseleave=${taskEventHandler}>
                 <input type='checkbox' class='ui checkbox' data-id='${task.id}' ?checked=${task.completed} @click=${checkboxEventHandler}>
-                <div class='task-title' contenteditable="true" data-id='${task.id}' @input=${updateEventHandler}>${task.title}</div>
+                <input type='text' class='task-title' contenteditable="true" data-id='${task.id}' @input=${updateEventHandler} value='${task.title}'>
                 <span id='actions' class='hidden actions'>
                     <i class='icon delete' data-id='${task.id}' @click=${deleteEventHandler}></i>
                 </span>
