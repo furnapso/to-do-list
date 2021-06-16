616db5932ea6356ae4b049edb5a240695e5e0804
furnapso <luke.boland95@gmail.com>
2021-06-16 15:13:41 +1000

    feat: add local storage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ import {render} from "lit-html";
 import Board from "./board";
 import UI from "./ui.js";
 
-Board.projects[0].addTask("New task", "Task description", new Date(), 1);
-Board.projects[0].addTask("New task", "Task description", new Date(), 1);
-Board.activeProject()._tasks[0].completed = true;
+let storage = window.localStorage;
+
+if (storage.getItem('board') !== null){
+    Board = JSON.parse(storage.getItem('board'))
+}
+
+else {
+    Board.projects[0].addTask("New task", "Task description", new Date(), 1);
+    Board.projects[0].addTask("New task", "Task description", new Date(), 1);
+    Board.activeProject()._tasks[0].completed = true;
+}
+
 UI(Board).draw();
-console.log(Board);
+
+window.setInterval(() => {
+    console.log('Saving to local storage')
+    storage.setItem('board', JSON.stringify(Board))
+}, 3000);
